9a1a49b7e101c197d4dc7481a246fbf60b440e87
Priscilla Standard <scylla@MacBook-Pro.local>
2020-07-21 19:43:37 -0400

    calm down

--- game.js
+++ game.js
@@ -2,11 +2,10 @@ const readline = require("readline-sync");
 
 function playAgain() {
   if (readline.keyInYN(`Play again? `) == false) {
-    console.log(`Bye, pathetic loser.`)
-    process.exit()
+    console.log(`Bye, pathetic loser.`);
+    process.exit();
   } else gameStart();
 }
-
 
 //  ROUND O
 
@@ -20,42 +19,49 @@ Enslave these disgusting humans, but DO NOT annihilate them! Also, don't die (lo
 
 let defaultName = `Ass-Face`;
 
-let humanSpy = `WHATT?!? NO! How has a pathetic human infiltrated our Intergalactic Armada?? DIE, HUMAN!!!!!!!`;
+let humanSpy = `WHATT?!? NO! How has a pathetic human infiltrated our Intergalactic Armada?? DIE, HUMAN!!!!!`;
 
 console.log(introScript);
 
 getPlayerName = () => {
-  playerName = readline.question(`What's your name, being? ` , {defaultInput: defaultName});
-    if (playerName == defaultName || 
-        playerName == null || 
-        playerName == undefined) {
-        console.log(`Really? You don't have a name? Okay then, your name is ${defaultName}.`);
-        playerName = defaultName;
-    } else {
-        console.log(`${playerName}! Outstanding! A name becoming of an intergalactic dictator-to-be.`);
-    }   
+  playerName = readline.question(`What's your name, being? `, {
+    defaultInput: defaultName,
+  });
+  if (
+    playerName == defaultName ||
+    playerName == null ||
+    playerName == undefined
+  ) {
+    console.log(
+      `Really? You don't have a name? Okay then, your name is ${defaultName}.`
+    );
+    playerName = defaultName;
+  } else {
+    console.log(
+      `${playerName}! Outstanding! A name becoming of an intergalactic dictator-to-be.`
+    );
   }
+};
 
 getPlayerName();
 
-
 getPlayerPlanet = () => {
-  if ((readline.question(`And from whence do you hail, ${playerName}?`.toLowerCase()) == "earth")) {
-      console.log(humanSpy);
-      setTimeout(playAgain(), 3000);
-  }  else return playerPlanet;
-}
+  if (
+    readline.question(
+      `And from whence do you hail, ${playerName}?`.toLowerCase()
+    ) == "earth"
+  ) {
+    console.log(humanSpy);
+    setTimeout(playAgain(), 3000);
+  } else return playerPlanet;
+};
 
 getPlayerPlanet();
 
-
 // ROUND 1
-
 
 let rdOneScript = `How do you wish to initiate your offensive, ${global.namePlayer} of ${global.planetPlayer}?`;
 
-
-
 function begOfRdOne() {
-     console.log(rdOneScript);
+  console.log(rdOneScript);
 }
